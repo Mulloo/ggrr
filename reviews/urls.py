263f8c0446b5531a73210da2f4f31fcf5f389dd3
Daniel Mullooly <mulloolyd.1998@gmail.com>
2024-07-03 03:03:24 +0000

    chore: refactored html to extend base | moved static files for home and base to main directory

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,8 +3,7 @@ from . import views
 from .views import home_page
 
 urlpatterns = [
-    path("", home_page, name="home_page"),
-    path("main/", views.main_page, name="main_page"),
+    path("", views.home_page, name="home_page"),
     path('equipment/<int:equipment_id>/', views.equipment_detail, name='equipment_detail'),
     path('equipment/<int:equipment_id>/add_review/', views.add_review, name='add_review'),
 ]
